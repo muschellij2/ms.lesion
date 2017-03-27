1da794549594e221eca1d96cd0fdd1e3a60f8fa3
muschellij2 <muschellij2@gmail.com>
2017-03-27 16:48:43 -0400

    Model is in data.

---
+++ R/ms_model.R
@@ -0,0 +1,7 @@
+#' @title Estimated Logistic Regression Model for MS
+#'
+#' @description A list containing the GLM 
+#' for predicting MS lesions at a voxel level
+#'
+#' @format A \code{glm} object with estimates.
+"ms_model"
