b1f0670a4a822bee439d664d89730e59fa8905eb
Kristin <kalinn@ncsu.edu>
2017-03-27 14:42:26 -0400

    oasis script

--- inst/extdata/run_oasis.R
+++ inst/extdata/run_oasis.R
@@ -8,55 +8,50 @@ all.exists = function(...){
   all(file.exists(...))
 }
 
-files = get_image_filenames_list_by_subject(type="coregistered")
+files = get_image_filenames_list_by_subject(type="coregistered", group="training")
 isubj = 1
-oasis_dfs = list()
+oasis_dfs_train = list()
 for (isubj in seq_along(files)) {
   print(isubj)
   fnames = files[[isubj]]
   id = names(files)[isubj]
-  outdir = file.path("coregistered",
-                     id)
-  dir.create(outdir)
-  outfiles = file.path(outdir, 
-                       basename(fnames))
+  outdir = file.path("models")
+
   t1_fname = fnames["MPRAGE"]
   t2_fname = fnames["T2"]
   flair_fname = fnames["FLAIR"]
   pd_fname = fnames["PD"]
-  maskfile = file.path(outdir,
-                       "brain_mask.nii.gz")
-  lesionfile = file.path(outdir,
-                       "mask2.nii.gz")
+  maskfile = fnames["Brain_Mask"]
+  lesionfile = fnames["mask2"]
+
   T1 = readnii(t1_fname)
   T2 = readnii(t2_fname)
   FLAIR = readnii(flair_fname)
   PD = readnii(pd_fname)
-  if(file.exists(maskfile)){
-    GOLD_STANDARD = readnii(lesionfile)
-    MASK = readnii(maskfile)
+  GOLD_STANDARD = readnii(lesionfile)
+  MASK = readnii(maskfile)
 
-    oasis_dfs = list(oasis_dfs, oasis_train_dataframe(flair = FLAIR, 
+  outfile = file.path(outdir, paste0(id, "_oasis_df.Rda"))
+
+  if(!file.exists(outfile)){
+    df = oasis_train_dataframe(flair = FLAIR, 
       t1 = T1, t2 = T2, pd = PD, gold_standard = GOLD_STANDARD,
       brain_mask = MASK, preproc = FALSE, normalize = TRUE,
-      return_preproc = FALSE))
+      return_preproc = FALSE)
+    save(df, file=outfile)
+  } else{
+    load(outfile)
   }
+
+  oasis_dfs_train = list(oasis_dfs_train, df)
+  rm(list="df")
 }
 
-  all_dfs = paste(unlist(lapply(1:5, function(x) paste0("oasis_dfs[[",x,"]]"))), collapse=",")
-  oasis_model = oasis_training(eval(parse(text=all_dfs)))
-  save.image()
+  model = do.call("oasis_training", oasis_dfs_train)
+  # paste(unlist(lapply(1:5, function(x) paste0("oasis_dfs[[",x,"]]"))), collapse=",")
+  save(model, file="models/oasis_model.Rda")
   
-  if (!all.exists(outfiles, maskfile)) {
-    preprocess_mri_within(
-      files = fnames,
-      outfiles = outfiles,
-      correction = "N4",
-      maskfile = maskfile,
-      correct_after_mask = TRUE)
-  }
-  # for (ifile in outfiles) {
-  #   print(ifile)
-  #   bias_correct(file = ifile, correction = "N4", outfile = ifile)
-  # }
 }
+
+
+
