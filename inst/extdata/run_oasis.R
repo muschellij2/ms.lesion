c9173f658f9c6b8752e174a1a23b2aaceccdc159
Kristin Linn <klinn@upenn.edu>
2017-03-28 01:21:39 -0400

    add predictions

--- inst/extdata/run_oasis.R
+++ inst/extdata/run_oasis.R
@@ -61,4 +61,48 @@ save(ms_model, file = "../../data/ms_model.rda",
      compression_level = 9)
 
 
+test_files = get_image_filenames_list_by_subject(
+  type = "coregistered", 
+  group = "testing")
 
+for (isubj in seq_along(test_files)) {
+  print(isubj)
+  fnames = files[[isubj]]
+  id = names(files)[isubj]
+  outdir = file.path("coregistered", id)
+  
+  t1_fname = fnames["MPRAGE"]
+  t2_fname = fnames["T2"]
+  flair_fname = fnames["FLAIR"]
+  pd_fname = fnames["PD"]
+  maskfile = fnames["Brain_Mask"]
+  
+  T1 = readnii(t1_fname)
+  T2 = readnii(t2_fname)
+  FLAIR = readnii(flair_fname)
+  PD = readnii(pd_fname)
+  MASK = readnii(maskfile)
+  
+  # Using default params
+  outfile = file.path(outdir, paste0(id, "_Default_OASIS"))
+  if (!file.exists(outfile)) {
+    map = oasis_predict(
+      flair = FLAIR, 
+      t1 = T1, t2 = T2, pd = PD, 
+      brain_mask = MASK, preproc = FALSE, normalize = TRUE,
+      model=oasis::oasis_model)
+    writenii(map, filename=outfile)
+  }
+
+  # Using re-trained model
+  outfile = file.path(outdir, paste0(id, "_Trained_OASIS"))
+  if (!file.exists(outfile)) {
+    map = oasis_predict(
+      flair = FLAIR, 
+      t1 = T1, t2 = T2, pd = PD, 
+      brain_mask = MASK, preproc = FALSE, normalize = TRUE,
+      model=ms.lesion::ms_model)
+    writenii(map, filename=outfile)
+  }
+ } 
+  
